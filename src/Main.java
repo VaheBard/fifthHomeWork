afa39db8d2318e8aa25f277fdace2a5a75f6d731
Vahe <vabardeg@gmail.com>
2023-03-05 12:14:58 +0400

    fiall commit

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,27 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int nameLength = 0;
-        int maxNameLength = 0;
         String maxName = null;
-        while(true){
+        while (true) {
             System.out.println("Enter a name with a capital letter: ");
             String input = sc.nextLine();
 
-            if(input.equals("end")){
+            if (input.equals("end")) {
                 System.out.println("The program is finished!");
                 break;
-            } else if(!Character.isUpperCase(input.charAt(0))){
+            } else if (!Character.isUpperCase(input.charAt(0))) {
                 System.out.println("Names should be entered with a capital letter\n");
-            } else if(nameLength == 0){
+            } else if (nameLength == 0) {
                 maxName = input;
                 nameLength = maxName.length();
                 System.out.println("It is the first name you entered!\n");
-            } else if(nameLength < input.length()){
+            } else if (nameLength < input.length()) {
                 nameLength = input.length();
                 maxName = input;
-                System.out.println("This name " + maxName + " is longest name!");
+                System.out.println("This name " + maxName + " is longest name!\n");
             } else {
-                maxNameLength = nameLength - input.length();
-                System.out.println("The longest name is " + maxName + " was not enough "+ maxNameLength + " characters");
+                int maxNameLength = nameLength - input.length();
+                System.out.println("The longest name is " + maxName + " was not enough " + maxNameLength + " characters\n");
             }
         }
     }
